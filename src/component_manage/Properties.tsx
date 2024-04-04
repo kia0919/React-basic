50e83d6d663299a37eeb6a2c5e797c8aea0d0bbb
kia0919 <john0256@naver.com>
2024-04-04 11:02:21 +0900

    2024.04.04

--- src/component_manage/Properties.tsx
+++ src/component_manage/Properties.tsx
@@ -5,11 +5,13 @@ import React from 'react'
 // - 부모 컴포넌트에서는 HTML과 동일한 방식 (속성명 = 데이터)로 전달
 // - 자식 컴포넌트에서는 JS의 매개변수 방식으로 받음
 // - 전달할 수 있는 데이터는 JS로 표현할 수 있는 모든 형태
-interface Props {
+interface Props {   
+    // title, content:Props의 속성
     title: string;
-    content: string
+    content: string;
 }
 
+//Child 함수형 컴포넌트props 매개변수를 받아 전달된 속성을 참조
 function Child(props: Props) {
 
     // props = {
@@ -17,6 +19,7 @@ function Child(props: Props) {
     //     content: '내용1'
     // }
 
+    // 함수형은 this가 없어서 props 매개변수를 직접 받아 사용
     return (
         <div>
             <h1>{props.title}</h1>
