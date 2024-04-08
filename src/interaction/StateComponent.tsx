cbc3d1d7ab8dd3254568bb07b0b5efbd7dabaf1b
kia0919 <john0256@naver.com>
2024-04-08 12:41:58 +0900

    2024.04.08

--- src/interaction/StateComponent.tsx
+++ src/interaction/StateComponent.tsx
@@ -19,6 +19,7 @@ export default function StateComponent() {
     const [total, setTotal] = useState<number>(0);
     // let favorites: string[] = [];, string 타입이므로 초기값은 빈 값
     const [favorites, setFavorites] = useState<string[]>(['사과'])
+    // 초기값은 빈 문자열.
     const [favorite, setFavorite] = useState<string>('');
 
     const onCountAddHandler = () => {
@@ -63,16 +64,16 @@ export default function StateComponent() {
 
     const onAddListHandler = () => {
         // 상태가 배열 혹은 객체이면 각각에 대해 요소를 추가, 변경이 일어나도, 
-        // 상태가 변경된 것으로 인식하지 않음 (실제 주소가 변경되지 않았기 때문)
+        // 상태가 변경된 것으로 인식하지 않음 (실제 주소가 변경되지 않았기 때문).
         // 상태가 변경된 것으로 인식하게 하고 싶으면 새로운 배열 혹은 객체를 생성하여,
-        // 상태를 변경해야 함 (일반적으로 복사해서 변경)
+        // 상태를 변경해야 함 (일반적으로 복사해서 변경).
 
         // 첫번째 방법
         // favorites.push(favorite);
         // const newFavorites = favorites.map(item => item);
         // setFavorites(newFavorites);  // 배열을 새로추가 (주소를 바꾸는 방법)
 
-        // 2번째 스프레드 방법 //위랑 같은 거지만 용도에 따라 사용.
+        // 2번째 스프레드(나머지) 방법 / 위랑 같은 거지만 용도에 따라 사용.
         setFavorites([...favorites, favorite]);
         setFavorite('');
     };
@@ -85,10 +86,15 @@ export default function StateComponent() {
         <hr/>
 
         {/* input이 만약 상태를 변경한다면 value로 그 상태를 지정해야 불일치가 발생하지 않음 */}
+        {/* inputBox에 입력 값이 바뀌면 바뀐 상태값이 화면에 표시된다.   */}
         <input onChange={onInputHandler} value={favorite}/>
+        {/* button요소의 onClick이벤트 생성하여 onAddListHandler함수를 호출 입력한 값을 favorite배열에 추가하여 화면에 표시 */}
         <button onClick={onAddListHandler}>추가</button>
+        {/* 상태변수의 값(입력한 값) 실시간으로 화면에 표시 */}
         <h4>{favorite}</h4>
         <ul>
+            {/* map 메서드로 item요소와 index를 반복 작업 */}
+            {/* 각 반복마다 li 태그 생성, 각 요소의 값을 item요소로 통해 화면에 표시 */}
             {favorites.map((item, index) => <li key={index}>{item}</li>)}
         </ul>
     </>    
